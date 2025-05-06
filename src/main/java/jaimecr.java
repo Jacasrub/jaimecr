eea85b1801433623c8f4dc89c2e1fc04a47d1b78
jaime <jaime.casanovarubio@educa.madrid.org>
2025-05-06 11:08:20 +0200

    Practica entornos

--- src/main/java/jaimecr.java
+++ src/main/java/jaimecr.java
@@ -2,14 +2,18 @@ public class jaimecr {
     public static void main(String[] args) {
         int i = 1;
         while (i <= 10) {
-            if (i % 2 == 0) {
-                System.out.println("El número " + i + " es par");
-            } else {
-                System.out.println("El número " + i + " es impar");
-            }
+            extracted(i);
             i = i + 1;
         }
 
 
+    }
+
+    private static void extracted(int i) {
+        if (i % 2 == 0) {
+            System.out.println("El número " + i + " es par");
+        } else {
+            System.out.println("El número " + i + " es impar");
+        }
     }
 }
