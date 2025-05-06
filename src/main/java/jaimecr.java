f3899347fd2a878571a5e63b19f11ced1c4afaf9
jaime <jaime.casanovarubio@educa.madrid.org>
2025-05-06 10:06:00 +0200

    Practica entornos

---
+++ src/main/java/jaimecr.java
@@ -0,0 +1,25 @@
+public class jaimecr {
+    public static void main(String[] args) {
+        int x=1;
+        System.out.println(x);
+        x=2;
+        System.out.println(x);
+        x=3;
+        System.out.println(x);
+        x=4;
+        System.out.println(x);
+        x=5;
+        System.out.println(x);
+        x=6;
+        System.out.println(x);
+        x=7;
+        System.out.println(x);
+        x=8;
+        System.out.println(x);
+        x=9;
+        System.out.println(x);
+        x=10;
+        System.out.println(x);
+
+    }
+}
